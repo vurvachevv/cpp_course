4a7c4886e01a7f71685da44212f52f4edbcaa6b5
vurvachevv <vurvachevv@mail.ru>
2024-09-14 03:13:22 +0300

    task_01 added

--- term1/homework_01/task_01.c
+++ term1/homework_01/task_01.c
@@ -2,7 +2,7 @@
 
 int main() {
     int num;
-    scanf("%d", num);
+    scanf("%d", &num);
     
     if (num % 2 == 0 && ((num >= 0 && num <= 20) || (num > 100 && num < 200))) {
         printf("Yes\n");
